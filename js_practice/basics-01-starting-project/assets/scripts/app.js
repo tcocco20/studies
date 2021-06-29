fb4c17b57380c8fc52d2c3682f63988edc36b4fd
Theo <theococco@gmail.com>
2021-06-29 15:13:13 -0700

    learning functions

--- js_practice/basics-01-starting-project/assets/scripts/app.js
+++ js_practice/basics-01-starting-project/assets/scripts/app.js
@@ -1,8 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = currentResult + 10 * 3 / 2 - 1;
+currentResult = currentResult + (10 * 3) / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10)  * 3 / 2 - 1`;
+
+add = (x, y) => {
+  result = x + y;
+  alert('The result is: ' + result)
+}
+
+add(5, 6);
 
 outputResult(currentResult, calculationDescription);
---
+++ js_practice/basics-assignment-1-start/assignment.js
@@ -0,0 +1,10 @@
+const user = 10;
+let result;
+result = 18 + user;
+
+result -= 2;
+result *= 2;
+result /= 2;
+
+alert(result)
+alert(user)
