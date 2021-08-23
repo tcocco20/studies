cd30bad91fd6244e04fec2e1e5095f6a2de286ed
Theo <theococco@gmail.com>
2021-08-23 15:51:45 -0700

    added OOP version of grading lab

--- python_practice/grading.py
+++ python_practice/grading.py
@@ -4,12 +4,12 @@ Grading Lab
 Last Updated: August 10th, 2021
 '''
 
-#Version 1 Simple Grade conversion
+# Version 1 Simple Grade conversion
 
 '''
 try:
 	score = int(input('Enter the numeric score you got: '))
-except:	
+except:
 	print('Please enter a number and try again')
 
 def get_grade(score):
@@ -23,18 +23,18 @@ def get_grade(score):
 		return "D"
 	else:
 		return "F"
-	
+
 print(f'Your grade is {get_grade(score)}')
 '''
 
-#Version 2 Adding + or -
+# Version 2 Adding + or -
 
 '''
 try:
 	score = int(input('Enter the numeric score you got: '))
-except:	
+except:
 	print('Please enter a number and try again')
-	
+
 def get_grade(score):
 	if score >= 90:
 		return "A"
@@ -46,7 +46,7 @@ def get_grade(score):
 		return "D"
 	else:
 		return "F"
-		
+
 def get_decorator(score):
 	if score % 10 <= 3:
 		return "-"
@@ -54,19 +54,44 @@ def get_decorator(score):
 		return "+"
 	else:
 		return ""
-		
+
 print(f'Your grade is {get_grade(score)}{get_decorator(score)}')
 '''
 
-#OOP Version
+# OOP Version
 
 class Grade:
-	def __init__(self, score):
-		self.score = score
+  def __init__(self, score):
+    self.score = score
+    
+  def get_grade(self, score):
+    if score >= 90:
+      return "A"
+    elif score >= 80:
+      return "B"
+    elif score >= 70:
+      return "C"
+    elif score >= 60:
+      return "D"
+    else:
+      return "F"
+    
+  def get_decorator(self, score):
+    if score % 10 <= 3:
+      return "-"
+    elif score % 10 >= 7:
+      return "+"
+    else:
+      return ""
+    
+  def __str__(self):
+  			return f'Your grade is {self.get_grade(self.score)}{self.get_decorator(self.score)}'
 
 try:
 	score = int(input('Enter the numeric score you got: '))
-except:	
+except:
 	print('Please enter a number and try again')
-	
 
+grade = Grade(score)
+
+print(grade)
