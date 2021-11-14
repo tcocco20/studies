d43dda8e10268e2aa986da475192ced06462dc34
Theo <theococco@gmail.com>
2021-11-13 21:46:25 -0800

    adding latest change

--- python_practice/django/django_refresh/password_generator/generator/views.py
+++ python_practice/django/django_refresh/password_generator/generator/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def home(request):
   return render(request, 'generator/home.html')
+
+def password(request):
+  characters = list('abcdefghijklmnopqrstuvwxyz')
+  
+  length = 10
+  
+  the_password = ''
+  
+  for x in range(length):
+    the_password = random.choice(characters)
+  
+  return render(request, 'generator/password.html', {'password':the_password})
