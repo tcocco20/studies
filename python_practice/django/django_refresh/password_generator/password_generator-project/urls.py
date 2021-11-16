e94eb3286d2ace390484441d9b3ea42be591f5a1
Theo Rafael Cocco <theococco@yahoo.com>
2021-11-16 09:52:49 -0800

    added bootstrap to django project

--- python_practice/django/django_refresh/password_generator/password_generator-project/urls.py
+++ python_practice/django/django_refresh/password_generator/password_generator-project/urls.py
@@ -20,4 +20,5 @@ from generator import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('password', views.password, name='password'),
+		path('about', views.about, name='about')
 ]
