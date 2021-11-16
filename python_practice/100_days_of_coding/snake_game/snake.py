fb3c70a8e73bd112ea989688ecdf0aabc0776064
Theo <theococco@gmail.com>
2021-11-16 09:08:00 -0800

    added controls to snake game

--- python_practice/100_days_of_coding/snake_game/snake.py
+++ python_practice/100_days_of_coding/snake_game/snake.py
@@ -2,11 +2,16 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake():
   def __init__(self):
     self.segments = []
     self.create_snake()
+    self.head = self.segments[0]
     
   def create_snake(self):
     for x in STARTING_POSITIONS:
@@ -21,5 +26,21 @@ class Snake():
       x = self.segments[s - 1].xcor()
       y = self.segments[s - 1].ycor()
       self.segments[s].goto(x, y)
-    self.segments[0].forward(MOVE_DISTANCE)
+    self.head.forward(MOVE_DISTANCE)
+  
+  def up(self):
+    if self.head.heading() != DOWN:
+      self.head.setheading(90)
+    
+  def down(self):
+    if self.head.heading() != UP:
+      self.head.setheading(270)
+    
+  def left(self):
+    if self.head.heading() != RIGHT:
+      self.head.setheading(180)
+    
+  def right(self):
+    if self.head.heading() != LEFT:
+      self.head.setheading(0)
 
