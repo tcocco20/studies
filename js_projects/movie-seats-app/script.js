08e5fda4c3007ac145479681623112be129cc868
Theo Rafael Cocco <theococco@yahoo.com>
2021-11-20 03:14:05 -0800

    adding latest update to projects

--- js_projects/movie-seats-app/script.js
+++ js_projects/movie-seats-app/script.js
@@ -3,11 +3,42 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+populateUI()
+
 let ticketPrice = +movieSelect.value;
+
+//get data from localstorage and populate UI
+function populateUI() {
+	const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+	if (selectedSeats !== null && selectedSeats.length > 0) {
+		seats.forEach((seat, index) => {
+			if (selectedSeats.indexOf(index) > -1) {
+				seat.classList.add('selected')
+			}
+		})
+	}
+	
+	const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+	
+	if (selectedMovieIndex !== null) {
+		movieSelect.selectedIndex = selectedMovieIndex
+	}
+}
+
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+	localStorage.setItem('selectedMovieIndex', movieIndex);
+	localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 // Update total and count
 function updateSelectedCount() {
 	const selectedSeats = document.querySelectorAll('.row .seat.selected');
+	
+	const seatsIndex = [...selectedSeats].map((seat) =>  [...seats].indexOf(seat));
+	
+	localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 	
 	const selectedSeatsCount = selectedSeats.length;
 	
@@ -18,6 +49,7 @@ function updateSelectedCount() {
 // Movie select event
 movieSelect.addEventListener('change', e => {
 	ticketPrice = +e.target.value;
+	setMovieData(e.target.selectedIndex, e.target.value)
 	updateSelectedCount();
 })
 
@@ -28,3 +60,6 @@ container.addEventListener('click', e => {
 		updateSelectedCount();
 	}
 });
+
+//initial count and total set
+updateSelectedCount();
