b5f478ec40989894224f4ac335f8534be12f368b
Theo <theococco@gmail.com>
2021-11-20 03:07:22 -0800

    started new portfolio django project

---
+++ python_practice/django/django_refresh/personal_portfolio/personal_portfolio_project/portfolio/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='portfolio/images/')
+    urls = models.URLField(blank=True)
