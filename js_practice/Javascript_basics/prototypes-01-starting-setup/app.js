05ad3072c1e3f25a410699e6f07bae7bcfff7aa3
Theo <theo@cocco-maddyscoding.dev>
2022-04-11 15:46:26 -0700

    added prototype examples

---
+++ js_practice/Javascript_basics/prototypes-01-starting-setup/app.js
@@ -0,0 +1,43 @@
+// class AgedPerson {
+//   printAge() {
+//     console.log(this.age);
+//   }
+// }
+
+// class Person extends AgedPerson {
+//   name = "Theo";
+
+//   constructor() {
+//     super();
+//     this.age = 28;
+//   }
+
+//   greet() {
+//     console.log(`Hi, I am ${this.name} and I am ${this.age} years old`);
+//   }
+// }
+
+function Person() {
+  this.age = 30;
+  this.name = "Theo";
+  this.greet = function () {
+    console.log(`Hi, I am ${this.name} and I am ${this.age} years old`);
+  };
+}
+
+// Person.prototype = {
+//   printAge() {
+//     console.log(this.age);
+//   },
+// };
+
+Person.prototype.printAge = function () {
+  console.log(this.age);
+};
+
+const p = new Person();
+p.greet();
+p.printAge();
+console.log(p.__proto__);
+const p2 = new p.__proto__.constructor();
+console.log(p2);
