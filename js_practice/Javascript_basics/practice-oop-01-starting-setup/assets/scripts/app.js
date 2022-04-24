a2cda90397479c06e9630d994a22e6e3f9c253d9
Theo <theo@cocco-maddyscoding.dev>
2022-04-23 18:51:53 -0700

    added sizes html, css, and js files, added data attributes for tooltip

--- js_practice/Javascript_basics/practice-oop-01-starting-setup/assets/scripts/app.js
+++ js_practice/Javascript_basics/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -36,9 +36,10 @@ class Component {
 }
 
 class Tooltip extends Component {
-  constructor(closeNotifierFunction) {
+  constructor(closeNotifierFunction, text) {
     super();
     this.closeNotifier = closeNotifierFunction;
+    this.text = text;
     this.create();
   }
   closeTooltip = () => {
@@ -49,7 +50,7 @@ class Tooltip extends Component {
   create() {
     const tooltipElement = document.createElement("div");
     tooltipElement.className = "card";
-    tooltipElement.textContent = "DUMMY!";
+    tooltipElement.textContent = this.text;
     tooltipElement.addEventListener("click", this.closeTooltip);
     this.element = tooltipElement;
   }
@@ -69,9 +70,11 @@ class ProjectItem {
     if (this.hasActiveTooltip) {
       return;
     }
+    const projectElement = document.getElementById(this.id);
+    const tooltipText = projectElement.dataset.extraInfo;
     const tooltip = new Tooltip(() => {
       this.hasActiveTooltip = false;
-    });
+    }, tooltipText);
     tooltip.attach();
     this.hasActiveTooltip = true;
   }
@@ -81,7 +84,7 @@ class ProjectItem {
     const moreInfoBtn = projectItemElement.querySelector(
       "button:first-of-type"
     );
-    moreInfoBtn.addEventListener("click", this.showMoreInfoHandler);
+    moreInfoBtn.addEventListener("click", this.showMoreInfoHandler.bind(this));
   }
 
   connectSwitchButton(type) {
