fd1d7e62401b3763cbc29ba28398a9c3686dcbc5
Theo <theo@cocco-maddyscoding.dev>
2022-04-23 18:35:42 -0700

    finished oop practice

--- js_practice/Javascript_basics/practice-oop-01-starting-setup/assets/scripts/app.js
+++ js_practice/Javascript_basics/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -12,26 +12,46 @@ class DOMHelper {
   }
 }
 
-class Tooltip {
+class Component {
+  constructor(hostElementId, insertBefore = false) {
+    if (hostElementId) {
+      this.hostElement = document.getElementById(hostElementId);
+    } else {
+      this.hostElement = document.body;
+    }
+    this.insertBefore = insertBefore;
+  }
+  detach() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  attach() {
+    this.hostElement.insertAdjacentElement(
+      this.insertBefore ? "afterbegin" : "beforeend",
+      this.element
+    );
+  }
+}
+
+class Tooltip extends Component {
   constructor(closeNotifierFunction) {
+    super();
     this.closeNotifier = closeNotifierFunction;
+    this.create();
   }
   closeTooltip = () => {
     this.detach();
     this.closeNotifier();
   };
 
-  detach() {
-    this.element.remove();
-  }
-
-  attach() {
+  create() {
     const tooltipElement = document.createElement("div");
     tooltipElement.className = "card";
     tooltipElement.textContent = "DUMMY!";
     tooltipElement.addEventListener("click", this.closeTooltip);
     this.element = tooltipElement;
-    document.body.append(tooltipElement);
   }
 }
 
