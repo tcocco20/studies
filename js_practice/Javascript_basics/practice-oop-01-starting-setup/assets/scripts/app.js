d01fd05e1d155e3a6c1febe07d569f4f892f6c38
tcocco20 <theococco@yahoo.com>
2022-07-01 18:03:10 -0700

    adding recent changes

--- js_practice/Javascript_basics/practice-oop-01-starting-setup/assets/scripts/app.js
+++ js_practice/Javascript_basics/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -82,6 +82,7 @@ class ProjectItem {
     this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectMoreInfoButton();
     this.connectSwitchButton(type);
+    this.connectDrag();
   }
 
   showMoreInfoHandler() {
@@ -99,6 +100,18 @@ class ProjectItem {
     );
     tooltip.attach();
     this.hasActiveTooltip = true;
+  }
+
+  connectDrag() {
+    const item = document.getElementById(this.id);
+    document.getElementById(this.id).addEventListener('dragstart', event => {
+      event.dataTransfer.setData('text/plain', this.id);
+      event.dataTransfer.effectAllowed = 'move';
+    });
+
+    item.addEventListener('dragend', event => {
+      console.log(event);
+    })
   }
 
   connectMoreInfoButton() {
@@ -137,6 +150,41 @@ class ProjectList {
         new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type)
       );
     }
+    console.log(this.projects);
+    this.connectDroppable();
+  }
+
+  connectDroppable() {
+    const list = document.querySelector(`#${this.type}-projects ul`);
+
+    list.addEventListener('dragenter', event => {
+      if (event.dataTransfer.types[0] === 'text/plain'){
+        event.preventDefault();
+        list.parentElement.classList.add('droppable');
+      }
+    });
+
+    list.addEventListener('dragover', event => {
+      if (event.dataTransfer.types[0] === 'text/plain'){
+        event.preventDefault();
+      }
+    });
+
+    list.addEventListener('dragleave', event => {
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list){
+        list.parentElement.classList.remove('droppable');
+      }
+    })
+
+    list.addEventListener('drop', event => {
+      const prjId = event.dataTransfer.getData('text/plain');
+      if (this.projects.find(p => p.id === prjId)) {
+        return;
+      }
+      document.getElementById(prjId).querySelector('button:last-of-type').click();
+      list.parentElement.classList.remove('droppable');
+      // event.preventDefault(); <- Not required here but might be.
+    })
   }
 
   setSwitchHandlerFunction(switchHandlerFunction) {
@@ -167,7 +215,20 @@ class App {
     finishedProjectsList.setSwitchHandlerFunction(
       activeProjectsList.addProject.bind(activeProjectsList)
     );
+    
+    // const timerId = setTimeout(this.startAnalytics, 3000);
+
+    // document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+    //   clearTimeout(timerId);
+    // });
   }
+
+  // static startAnalytics() {
+  //   const analyticsScript = document.createElement('script');
+  //   analyticsScript.src = 'assets/scripts/analytics.js';
+  //   analyticsScript.defer = true;
+  //   document.head.append(analyticsScript);
+  // }
 }
 
 App.init();
